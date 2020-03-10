ad4cbc10d852d72ee9f436b1182be91090e4ceb7
Diego <diego.ochoa@globant.com>
2020-03-10 13:50:02 -0500

    User validation

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error('MongoDB Failed to Connect');
+    console.error(err.message);
+    //Exit proces with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
