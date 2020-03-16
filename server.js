83074b64306707d5d6661eb274bf793298fa4595
diego8arock <diegoa2000alt@gmail.com>
2020-03-15 19:48:42 -0500

    User registration

--- server.js
+++ server.js
@@ -19,4 +19,6 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+try {
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+} catch (error) {}
