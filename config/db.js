1fb9d90cd86f012b7de7636f9b5a347eec1912d9
Diego Ochoa <diego-ochoa@pop-os.localdomain>
2020-04-03 11:49:36 -0500

    Add connect retry

--- config/db.js
+++ config/db.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+const retries = 5;
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error('MongoDB Failed to Connect');
-    console.error(err.message);
-    //Exit proces with failure
+  let tries = 0;
+  let connected = false;
+  while (tries < retries) {
+    console.error('Trying to connect MongoDB: ' + tries);
+    try {
+      await mongoose.connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+      });
+      console.log('MongoDB Connected...');
+      connected = true;
+      tries = retries;
+    } catch (err) {
+      console.error('MongoDB Failed to Connect');
+      console.error(err.message);
+      tries++;
+    }
+  }
+  if (!connected) {
+    //Exit process with failure
     process.exit(1);
   }
 };
